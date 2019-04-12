7a92fb3f74c2e88bb4b70e661a0e6db96feb1835
Linda Yang <yanglinda24@gmail.com>
2019-04-12 13:36:29 -0400

    Imported UsernameForm component. Render UsernameForm and intergrated onUsernameSubmitted event handler. Send POST request when onUsernameSubmitted is called

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { Component } from 'react';
+import UsernameForm from './components/UsernameForm';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      currentUsername: '',
+    }
+    this.onUsernameSubmitted = this.onUsernameSubmitted.bind(this)
+  }
+
+  onUsernameSubmitted(username) {
+    fetch('http://localhost:8000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username }),
+    })
+      .then(response => {
+        this.setState({
+          currentUsername: username
+        })
+      })
+      .catch(error => console.error('error', error))
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>Chatly</h1>
+        <UsernameForm onSubmit={this.onUsernameSubmitted} />
       </div>
     );
   }
