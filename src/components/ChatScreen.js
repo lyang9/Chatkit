8966cb699b85fe937bd9bf61e7d5aec6d40e9694
Linda Yang <yanglinda24@gmail.com>
2019-04-14 15:16:33 -0400

    Created basic UI layout. Fixed typo for ChatManager

--- src/components/ChatScreen.js
+++ src/components/ChatScreen.js
@@ -10,7 +10,7 @@ class ChatScreen extends Component {
   }
 
   componentDidMount() {
-    const chatManager = new Chatkit.chatManager({
+    const chatManager = new Chatkit.ChatManager({
       instanceLocator: 'v1:us1:566b995c-0276-46fe-8b2f-1e69e0893135',
       userId: this.props.currentUsername,
       tokenProvider: new Chatkit.TokenProvider({
@@ -27,9 +27,41 @@ class ChatScreen extends Component {
   }
 
   render() {
+    const styles = {
+      container: {
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+      },
+      chatContainer: {
+        display: 'flex',
+        flex:  1,
+      },
+      whosOnlineListContainer: {
+        width: '300px',
+        flex: 'none',
+        padding: 20,
+        backgroundColor: '#2c303b',
+        color: 'white',
+      },
+      chatListContainer: {
+        padding: 20,
+        width: '85%',
+        display: 'flex',
+        flexDirection: 'column',
+      },
+    }
+
     return (
-      <div>
-        <h1>Chat</h1>
+      <div style={styles.container}>
+        <div style={styles.chatContainer}>
+          <aside style={styles.whosOnlineListContainer}>
+            <h2>Who's online PLACEHOLDER</h2>
+          </aside>
+          <section style={styles.chatListContainer}>
+            <h2>Chat PLACEHOLDER</h2>
+          </section>
+        </div>
       </div>
     );
   }
